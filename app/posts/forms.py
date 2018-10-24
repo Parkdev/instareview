92c992f6b003eaa9aa8b4b069e21ae19ef2142e5
Jeeho Park <devpark29@gmail.com>
2018-10-24 17:53:00 +0900

    form에 class속성추가위한 세팅

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -2,4 +2,20 @@ from django import forms
 
 
 class PostCreateForm(forms.Form):
-    photo = forms.ImageField()
+    photo = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'class': 'form-control-file'
+            }
+        )
+    )
+    comment = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'class' : 'form-control',
+            }
+        ),
+    )
+
+    #widget를 쓰는이유는 class를 적용하기 위해서이다?
