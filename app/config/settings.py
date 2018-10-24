7bc14132e401ebb2868a9a1c005dbb0c5d9efc47
Jeeho Park <devpark29@gmail.com>
2018-10-24 15:59:44 +0900

    template폴더 생성연결

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 MEDIA_ROOT = os.path.join(ROOT_DIR,'.media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
@@ -61,7 +62,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
