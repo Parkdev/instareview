96c9cad349150e891bbd315354b38a0f1563b032
Jeeho Park <devpark29@gmail.com>
2018-10-29 20:26:43 +0900

    index생성

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,9 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='post:post-list')),
     path('posts/', include('posts.urls')),
     path('members/', include('members.urls')),
 ]
