76e2e77b5e78d24615a1e5c939a560e2a01e8278
Jeeho Park <devpark29@gmail.com>
2018-10-29 19:41:06 +0900

    회원가입 수정

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -30,7 +32,16 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if username == User.objects.filter(username=username).exists():
+            return HttpResponse('존재하는 아이디입니다.')
+        elif password1 != password2:
+            return HttpResponse('비밀번호가 다릅니다.')
+        user = User.objects.create_user(username=username, password=password1)
+        login(request, user)
+        return redirect('posts:post-list')
     else:
         form = SignupForm()
         context = {
